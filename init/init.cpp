b9c7acb1f213d5901c2926736708c7024b9b67fd
David Greaves <david.greaves@jollamobile.com>
2014-11-25 19:00:37 +0100

    disable charger, queue_property_triggers and queue_device_triggers in init.rc

--- init/init.cpp
+++ init/init.cpp
@@ -1115,6 +1115,7 @@ int main(int argc, char** argv) {
 
     signal_handler_init();
 
+
     property_load_boot_defaults();
     start_property_service();
 
@@ -1153,7 +1154,7 @@ int main(int argc, char** argv) {
     queue_builtin_action(queue_property_triggers_action, "queue_property_triggers");
 
     /* run all device triggers based on current state of device nodes in /dev */
-    queue_builtin_action(queue_device_triggers_action, "queue_device_triggers");
+    //    queue_builtin_action(queue_device_triggers_action, "queue_device_triggers");
 
     /* Run actions when all boot up is done and init is ready */
     action_for_each_trigger("ready", action_add_queue_tail);
